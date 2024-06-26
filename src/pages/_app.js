2494f544e4a37842628922d9c2a7aec012eda073
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2024-06-26 17:14:26 -0400

    feat: Add GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,35 @@
+import ReactGA from "react-ga";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 import "../styles/WorkStyles.css";
 import "../styles/ProjectStyles.css";
 import "../styles/ThemeToggleStyles.css";
-
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect, useState } from "react";
 import Theme from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
   const [themeType, setThemeType] = useState("dark");
+  const router = useRouter();
 
   useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    const handleRouteChange = (url) => {
+      ReactGA.set({ page: url });
+      ReactGA.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
     AOS.init();
-  }, []);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <Theme themeType={themeType} setThemeType={setThemeType}>
